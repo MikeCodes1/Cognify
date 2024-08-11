bb3b6f71e8400d304fdabe25bafaa01cfdcbd297
MikeCodes1 <154788412+MikeCodes1@users.noreply.github.com>
2024-08-11 01:50:21 -0500

    firebase set up

--- FrontEnd/src/Components/LoginForm/LoginForm.jsx
+++ FrontEnd/src/Components/LoginForm/LoginForm.jsx
@@ -2,29 +2,61 @@ import { BrowserRouter,Routes,Route, Link } from 'react-router-dom';
 import React from 'react';
 import './LoginForm.css';
 import { FaUser, FaLock } from "react-icons/fa";
+import { auth, googleProvider } from '../../config/firebase'
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth' 
+import { useState } from "react";
 
-
+/* **CHANGE USERNAME TO EMAIL** */
 const LoginForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = async () =>{
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+        } catch(err) {
+            console.error(err);
+        }
+
+    };
+    const signInWithGoogle = async () => {
+        try{
+            await signInWithPopup(auth, googleProvider);
+        }catch (err){
+            console.error(err); 
+        }
+    }
+    const logout = async () => {
+        try{
+            await signOut(auth);
+        }catch (err){
+            console.error(err); 
+        }
+    }
+
     return(
         <div className='wrapper'>
             <form action="">
                 <h1>Login</h1>
                 <div className="input-box">
-                    <input type="text" placeholder='Username' required/>
+                    <input type="text" placeholder='Username' required 
+                    onChange={(e) => setEmail(e.target.value)}/>
                     <FaUser className ='icon'/>
                 </div>
                 <div className="input-box">
-                    <input type="password" placeholder='Password' required/>
+                    <input type="password" placeholder='Password' required
+                    onChange={(e) => setPassword(e.target.value)}/>
                     <FaLock className ='icon'/>
                 </div>
-
+                
                 <div className="remember-forgot">
                     <label><input type="checkbox" />Remember me</label>
                     <a href='#'>Forgot password?</a>
                 </div>
 
-                <Link to = "/HomePage"><button type="submit">Login</button></Link>
-
+                <Link to = "/HomePage"><button type="submit" onClick={signIn}>Login</button></Link>
+                <Link to = "/HomePage"><button type="submit" onClick={signInWithGoogle}> Sign In with Google</button> </Link>
+                <button onClick={logout}> Logout </button>
                 <div className="register-link">
                     <p>Don't have an account? <Link to ="/RegisterForm"> Register</Link></p>
                 </div>
---
+++ FrontEnd/src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {getAuth, GoogleAuthProvider} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore'
+const firebaseConfig = {
+  apiKey: "AIzaSyAXRLRgR8FUDcktaLYHsgg5OrvVtzTUhq8",
+  authDomain: "cognify-c0678.firebaseapp.com",
+  projectId: "cognify-c0678",
+  storageBucket: "cognify-c0678.appspot.com",
+  messagingSenderId: "80340697226",
+  appId: "1:80340697226:web:eee7656148c8431ea8ad73",
+  measurementId: "G-S6RGLDZNP8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
